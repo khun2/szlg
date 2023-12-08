c245797f53f659d0de3c6e02544c3a6d80b7469b
khun <khunor22@gmail.com>
2023-12-08 18:27:39 +0100

    asd

---
+++ 23-24/karesz/LogoKaresz1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LogoKaresz
+{
+	public partial class Form1 : Form
+	{
+		/* Függvények */
+
+
+		/* Függvények vége */
+		void FELADAT()
+		{
+			/* Ezt indítja a START gomb! */
+			// Teleport(közép.X, közép.Y+150, észak);
+
+			Előre(100);
+			Jobbra(90);
+			Előre(100);
+			Jobbra(100);
+            Előre(100);
+
+            ;
+
+        }
+	}
+}
