867f97c802807d91a5f385b31a8297b842cb428f
khun2 <khunor22@gmail.com>
2023-12-09 08:16:03 +0100

    asd

---
+++ 23-24/karesz/LKhímzés/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LogoKaresz
+{
+	public partial class Form1 : Form
+	{
+		/* Függvények */
+		void Négyzet(double a)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				Előre(a);
+				Jobbra(90);
+			}
+		}
+		void Négyzetbal(double a) {
+			for (int i = 0; i < 4; i++)
+			{
+				Előre(a);
+				Jobbra(-90);
+			}
+		}
+		void alapminta(double a)
+		{
+			Tollvastagság(3);
+			//Tollszín(Color.Red);
+			Jobbra(135);
+			Előre(a);
+			Jobbra(-90);
+			Előre(a);
+			Jobbra(-90);
+			Négyzet(a / 2);
+			Előre(a / 2);
+			//Négyzetbal(a / 2);
+		}
+		
+
+		
+
+		/* Függvények vége */
+		void FELADAT()
+		{
+			/* Ezt indítja a START gomb! */
+			 Teleport(közép.X, közép.Y, észak);
+			alapminta(50);
+
+		
+		}
+	}
+}
