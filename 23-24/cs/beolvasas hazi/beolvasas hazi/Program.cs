c5bda36472abe2c5c59b61e7af6f09b5f04b6070
khun2 <134009911+khun2@users.noreply.github.com>
2023-12-09 07:28:18 +0100

    Add files via upload

---
+++ 23-24/cs/beolvasas hazi/beolvasas hazi/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beolvasas_hazi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("kérek egy oldalt! ");
+            double oldal1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("kérek mégegy oldalt! ");
+            double oldal2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine((oldal1 + oldal2) * 2);
+            if (oldal1 == oldal2){
+                Console.WriteLine("a téglalap egy négyzet");
+                   }
+            else {
+                Console.WriteLine("a téglalap nem egy négyzet");
+                    }
+
+            Console.Write("kérek egy másik számot ");
+            double szam = Convert.ToDouble(Console.ReadLine());
+            if (szam % 7 == 0){
+                Console.WriteLine("a szám osztható héttel");
+            }
+            else {
+                Console.WriteLine("a szám nem osztható héttel");
+                    }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ 23-24/cs/beolvasás/beolvasás/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beolvasás
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            { Console.Write("szám1:");
+            double szam1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("az első szám:" + szam1);
+            Console.Write("szam2:");
+            double szam2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("a második szám:" + szam2);
+            Console.WriteLine("összeg: " + szam1 + szam2);
+            Console.WriteLine("szorzat: " + szam1 * szam2);
+            Console.WriteLine("modulo: " + szam1 % szam2);
+            Console.ReadLine();
+
+        }
+    }
+}
