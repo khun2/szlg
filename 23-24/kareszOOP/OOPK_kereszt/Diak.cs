dbbbd71a462513b3611cdfe1480a23e176052f50
e e <khunor22@gmail.com>
2023-12-14 12:23:32 +0100

    OOPkaresz 1

---
+++ 23-24/kareszOOP/OOPK_kereszt/Diak.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Karesz
+{
+    public partial class Form1 : Form
+    {
+        void DIÁK_ROBOTJAI()
+        {
+            Robot karesz = Robot.Get("Karesz");
+
+            karesz.Feladat = delegate ()
+            {
+                while (true)
+                {
+                    karesz.Lépj();
+                }
+
+            };
+        }
+    }
+}
+
+/* LEGFONTOSABB PARANCSOK
+
+MOZGÁSOK
+
+karesz.Lépj();                          -------- Karesz előre lép egyet.
+karesz.Fordulj(jobbra);                 -------- Karesz jobbra fordul.
+karesz.Fordulj(balra);                  -------- Karesz balra fordul.
+karesz.Vegyél_fel_egy_kavicsot();       -------- Karesz felvesz egy kavicsot.
+karesz.Tegyél_le_egy_kavicsot();        -------- Karesz letesz egy fekete kavicsot
+karesz.Tegyél_le_egy_kavicsot(piros);   -------- Karesz letesz egy piros kavicsot.
+
+Minden mozgás után a robot köre véget ér és a következő robot jön. 
+
+
+
+SZENZOROK
+
+karesz.Előtt_fal_van();                 -------- igaz, ha Karesz fal előtt áll, egyébként hamis.
+karesz.Ki_fog_lépni_a_pályáról();       -------- igaz, ha Karesz a pálya szélén kifele néz, egyébként hamis.
+karesz.Alatt_van_kavics();              -------- igaz, ha Karesz épp kavicson áll, egyébként hamis.
+karesz.Köveinek_száma_ebből(piros)      -------- Karesz piros köveinek a száma.
+karesz.Alatt_ez_van();                  -------- a kavics színe, amin Karesz áll. (Ez igazából egy szám!)
+karesz.UltrahangSzenzor();              -------- a Karesz előtt található tárgy távolsága. Ez a tárgy lehet fal vagy másik robot is. 
+karesz.SzélesUltrahangSzenzor();        -------- ugyanaz, mint az ultrahangszenzor, de ez nem csak a Karesz előtti mezőket pásztázza, hanem a szomszédos mezőket is. Egy számhármast ad vissza. 
+karesz.Hőmérő();                        -------- a Karesz által mért hőmérséklet. A láva forrása 1000 fok, amitől lépésenként távolodva a hőmérséklet 200 fokonként hűl. Az alapértelmezett hőmérséklet 0 fok.
+
+A szenzorokat bármennyiszer használhatja a robot a saját körén belül.
+
+*/
---
+++ 23-24/kareszOOP/OOPK_kereszt/Instanciamentes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Karesz
+{
+	partial class Form1
+	{
+        #region Robotokra visszavezetett parancsok
+
+        void Lépj() => 
+			Robot.akit_kiválasztottak.Lépj();
+		/*
+		void Fordulj_jobbra() => 
+			Robot.akit_kiválasztottak.Fordulj(jobbra);
+		void Fordulj_balra() => 
+			Robot.akit_kiválasztottak.Fordulj(balra);
+		*/
+		void Fordulj(int irány) => 
+			Robot.akit_kiválasztottak.Fordulj(irány);
+		int Köveinek_száma_ebből(int szín) => 
+			Robot.akit_kiválasztottak.Köveinek_száma_ebből(szín);
+		void Vegyél_fel_egy_kavicsot() => 
+			Robot.akit_kiválasztottak.Vegyél_fel_egy_kavicsot();
+		void Tegyél_le_egy_kavicsot(int szín = fekete) => 
+			Robot.akit_kiválasztottak.Tegyél_le_egy_kavicsot(szín);
+		bool Van_e_itt_Kavics() => 
+			Robot.akit_kiválasztottak.Alatt_van_kavics();
+		int Mi_van_alattam() => 
+			Robot.akit_kiválasztottak.Alatt_ez_van();
+		bool Van_e_előttem_fal() =>
+			Robot.akit_kiválasztottak.Előtt_fal_van();
+		bool Kilépek_e_a_pályáról() =>
+			Robot.akit_kiválasztottak.Ki_fog_lépni_a_pályáról();
+		int Ultrahang() => 
+			Robot.akit_kiválasztottak.UltrahangSzenzor();
+		int Hőmérséklet() => 
+			Robot.akit_kiválasztottak.Hőmérő();
+
+        #endregion
+
+        #region Pályára visszavezetett parancsok
+
+        void Betölt(string path) => pálya.Betölt(path);
+
+        #endregion
+	}
+}
