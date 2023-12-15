94b6566efd25c860feec3389439cfd204c937b28
khun <khunor22@gmail.com>
2023-12-15 08:19:06 +0100

    asd

--- 23-24/karesz/LKasztal/Form1.cs
+++ 23-24/karesz/LKasztal/Form1.cs
@@ -32,7 +32,7 @@ namespace LogoKaresz
 				Jobbra(90);
 				Előre(a);
 				Jobbra(90);
-				Előre(a / 4);
+				Előre(a / 3);
 				ívonal(a);
 				Tollat(le);
 		}
@@ -43,17 +43,53 @@ namespace LogoKaresz
 			}
 			Tollat(fel);
 			Jobbra(-90);
-			Előre(a * sorszam * 3 + sorszam * a / 4);
+			Előre(a * sorszam * 3 + sorszam * a / 3);
 			Jobbra(90);
 			Tollat(fel);
 		}
-		/* Függvények vége */
-		void FELADAT()
+        void Téglalap(double a, double b)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Előre(a);
+                Jobbra(90);
+                Előre(b);
+                Jobbra(90);
+            }
+        }
+        void Téglalap(double a, double b, Color color)
+        {
+            Téglalap(a, b);
+            double root = Math.Min(Math.Sqrt(2 * a), Math.Sqrt(2 * b));
+            Jobbra(45);
+            Tollat(fel);
+            Előre(root);
+            Tölt(color);
+            Előre(-root);
+            Jobbra(-45);
+            Tollat(le);
+        }
+		void asztal(double a, int oszlop, int sorszam, Color color1, Color color2) {
+			Tollszín(color1);
+			double téglalap1 = a * 3 * oszlop + (oszlop + 1) * a / 3;
+			double téglalap2 = a * 3 * sorszam + (sorszam + 1) * a / 3;
+            Téglalap(téglalap1, téglalap2, color2);
+			//ez nagyon ronda matek de én szeretem a nagyon ronda matekot mivel így lesz a legpontosabb
+			Tollat(fel);
+			Jobbra(90);
+			Előre(a + a / 3);
+			Jobbra(-90);
+			Előre(a/3);
+			Tollat(le);
+			sor(a, sorszam, color1);
+		}
+        /* Függvények vége */
+        void FELADAT()
 		{
 			/* Ezt indítja a START gomb! */
 			// Teleport(közép.X, közép.Y+150, észak);
-			
-			sor(20, 4, Color.Brown);
+
+			asztal(10, 4, 6,Color.Black, Color.White);
 		}
 	}
 }
