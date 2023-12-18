5808184b05a114c59b8c203848225ee6dd81000d
khun2 <khunor22@gmail.com>
2023-12-18 19:52:55 +0100

    Cipzar

--- 23-24/karesz/LKcipzar/Form1.cs
+++ 23-24/karesz/LKcipzar/Form1.cs
@@ -32,8 +32,7 @@ namespace LogoKaresz
 			}
 		}
 		Color Szinvaltas(Color color, Color color1, Color color2) {
-			color = color == color1 ? color2 : color1;
-			return color;
+			return(color = color == color1 ? color2 : color1);
 		}
 		void Oldalaz(double a) {
 			using (new Rajzol(false)) {
@@ -43,19 +42,67 @@ namespace LogoKaresz
 			}
 		}
 		void Sor(double size, int amount, Color color1, Color color2) {
-			Color color = Color.Aquamarine;
+			Color color = color2;
 			for (int i = 0; i < amount; i++){
-				Fog(size, Szinvaltas(color,color1, color2));
+				color=Szinvaltas(color, color1, color2);
+				Fog(size, color);
 				Oldalaz(size);
 			}
+			Oldalaz(-amount * size);
 		}
-		
+		void Sor2(double size, int amount, Color color1, Color color2) {
+            Oldalaz(amount * size);
+			Jobbra(180);
+			Sor(size, amount, color1, color2);
+            Jobbra(180);
+            Oldalaz(-amount * size);
+        }
+		void Dupla(double size, int amount, Color color1, Color color2, Color color3, Color color4) {
+            Sor(size, amount, color1, color2);
+			using (new Rajzol(false)) {
+				Előre(size);
+				Jobbra(30);
+				Előre(size);
+				Jobbra(-30);
+				Előre(size);
+			}
+			Sor2(size, amount, color3, color4);
+			using (new Rajzol(false)) {
+				Előre(-size);
+				Jobbra(30);
+				Előre(-size);
+				Jobbra(-30);
+				Előre(-size);
+			}
+        }
+		void Fal(double size, int amount,int height, Color color1, Color color2, Color color3, Color color4) {
+			for(int i = 0; i < height; i++) {
+				Dupla(size, amount, color1, color2, color3, color4);
+				using (new Rajzol(false)) {
+					Előre(size);
+					Jobbra(30);
+					Előre(size);
+					Jobbra(-30);
+					Előre(size);
+				}
+			
+            }
+            using (new Rajzol(false)) {
+                for (int i = 0; i < height; i++) {
+                    Előre(-size);
+                    Jobbra(30);
+                    Előre(-size);
+                    Jobbra(-30);
+                    Előre(-size);
+                }
+            }
+
+        }
         /* Függvények vége */
-        void FELADAT()
-		{
-			/* Ezt indítja a START gomb! */
-			// Teleport(közép.X, közép.Y+150, észak);
-			Sor(10,5, Color.Black, Color.White);
+        void FELADAT() { using (new Frissítés(false))
+        /* Ezt indítja a START gomb! */
+		// Teleport(közép.X, közép.Y+150, észak);
+        Fal(10, 10,4, Color.Red, Color.Blue, Color.Green, Color.Purple);
 		}
 	}
 }
