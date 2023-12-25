6c8f50b6080d0426ab56848611deb101df99fc4a
khun2 <khunor22@gmail.com>
2023-12-25 13:59:00 +0100

    asd

---
+++ 23-24/karesz/LKnyiluzio/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LogoKaresz
+{
+	public partial class Form1 : Form
+	{
+		/* Függvények */
+
+
+		/* Függvények vége */
+		void FELADAT()
+		{
+			/* Ezt indítja a START gomb! */
+			// Teleport(közép.X, közép.Y+150, észak);
+
+			Ív(90, 100);
+		}
+	}
+}
