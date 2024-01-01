6c10b090c8498ad3cf45f3fc02e08b5ce3e3e756
khun2 <khunor22@gmail.com>
2024-01-01 15:09:24 +0100

    a

--- 23-24/karesz/LKnyiluzio/Form1.cs
+++ 23-24/karesz/LKnyiluzio/Form1.cs
@@ -36,12 +36,8 @@ namespace LogoKaresz
                 Jobbra(-90);
             }
         }
-        void Triangle(double size) {
-            double root = 2*(Math.Sqrt(2 * size));
-            //Wolfram Alpha nem hazudik (ha helyesen írtam be az egyenletet)
-        }
         void ArrowHead(double size) {
-            double root = 2*(Math.Sqrt(2 * size));
+            double root = Math.Sqrt(2*size*size);
             Jobbra(90);
             Oldalaz(-size * 2);
             Rectangle(size * 2, size);
@@ -52,22 +48,85 @@ namespace LogoKaresz
             Rectangle(size * 3, size);
             Oldalaz(size);
             Előre(size);
+            Jobbra(22);
+            Tölt(root / 2,Color.Black);
+            Jobbra(-22);
         }
         void Alap(double size) {
-            double root = Math.Sqrt(2*size);
+            double root = Math.Sqrt(2*size*size);
             Jobbra(-45);
             Rectangle(size * 3,root);
             Előre(size * 3);
             Jobbra(45);
             ArrowHead(size);
+            Jobbra(-45);
+            Előre(-size * 3);
+            Jobbra(45);
+        }
+        void Nyílnégyzet(double size) {
+            double root = Math.Sqrt(2*size*size);
+            for (int i = 0; i < 4; i++) {
+                Alap(size);
+                Jobbra(45);
+                Előre(root);
+                Jobbra(45);
+            }
+            Jobbra(90);
+            Tölt(size/2,Color.Black);
+            Jobbra(-90);
+        }
+        void Swap(double size) {
+            using(new Rajzol(false)) {
+                double root = Math.Sqrt(2 * size * size);
+                Jobbra(45);
+                Előre(root + size * 3);
+                Oldalaz(root);
+                Jobbra(-45);
+                Oldalaz(2 * size);
+                Jobbra(45);
+                Oldalaz(size * 3);
+                Jobbra(-45);
+            }
+        }
+        void SwapFel(double size) {
+            double root = Math.Sqrt(2*size*size);
+            using (new Rajzol(false)) {
+                Jobbra(-135);
+                Oldalaz(-root);
+                Jobbra(45);
+                Swap(size);
+                Jobbra(45);
+                Előre(root);
+                Jobbra(45);
+            }
+        }
+        void Nyílsor(double size, int row) {
+            for (int i = 0; i < row; i++) {
+                Nyílnégyzet(size);
+                Swap(size);
+            }
+            Jobbra(180);
+            for (int i = 0;i < row; i++) {
+                Swap(size);
+            }
+            Jobbra(180);
+        }
+        void Nyílminta(double size, int row, int pillar) {
+            for(int i = 0;i<pillar ; i++){
+                Nyílsor(size, row);
+                SwapFel(size);
+            }
+            Jobbra(180);
+            for (int i = 0; i < pillar; i++) {
+                SwapFel(size);
+            }
+            Jobbra(180);
         }
         /* Függvények vége */
         void FELADAT() {
             /* Ezt indítja a START gomb! */
-            // Teleport(közép.X, közép.Y+150, észak);
-            //Alap(20);
-            Triangle(10);
-            
+            Teleport(közép.X/4, közép.Y*1.8, észak);
+            Nyílminta(10, 4, 3);
 		}
 	}
 }
