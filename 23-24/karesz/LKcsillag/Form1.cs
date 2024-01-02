3005083a3b2e93af85986cac5187422e8d289196
khun2 <khunor22@gmail.com>
2024-01-02 16:20:13 +0100

    asd

---
+++ 23-24/karesz/LKcsillag/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LogoKaresz
+{
+	public partial class Form1 : Form
+	{
+        /* Függvények */
+        void Tölt(double size, Color color)
+        {
+            using (new Rajzol(false))
+            {
+                Előre(size);
+                Tölt(color);
+                Előre(-size);
+            }
+        }
+        void Rombusz(double size, double angle,Color color) {
+            Jobbra(angle);
+            for (int i = 0; i < 2; i++){
+                Előre(size);
+                Jobbra(-angle);
+                Előre(size);
+                Jobbra(-(180-angle));
+            }
+            Jobbra(-angle/2);
+            Tölt(size/2, color);
+            Jobbra(-angle/2);
+        }
+        void Change(double size, double angle) {
+            using (new Rajzol(false)) {
+                Jobbra(angle);
+                Előre(size);
+                Jobbra(-angle);
+            }
+        }
+        void Row(double size,double angle, int length, Color[] colors) {
+            
+            for (int i = 0; i < length; i++) {
+                Rombusz(size,angle, colors[i]);
+                //ezt a Color[] color dolgot Petitől loptam(engedélyével ofc) mert teljesen el voltam akadva
+                Change(size,angle);
+            }
+            Change(-length*size,angle);
+        }
+        void Mozaik(double size, double angle, int length, Color[] colors) {
+            Color[] colors2 = colors;
+            for (int i = 0; i < length; i++) {
+                Row(size,angle,length,colors);
+                
+                Előre(size);
+            }
+        }
+        /* Függvények vége */
+        void FELADAT()
+		{
+            /* Ezt indítja a START gomb! */
+            // Teleport(közép.X, közép.Y+150, észak);
+            Mozaik(40, 45, 5, [Color.Aquamarine,Color.Green,Color.Blue,Color.Black,Color.White,Color.Pink]);
+			
+		}
+	}
+}
---
+++ 23-24/karesz/LKpiramis/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LogoKaresz
+{
+	public partial class Form1 : Form
+	{
+		/* Függvények */
+		void Deltoid(double size, double size1) {
+			for (int i = 0; i<2; i++) {
+				Előre(size1);
+				Jobbra(60);
+				Előre(size);
+				Jobbra(120);
+			}
+		}
+		void Brick(double size,double size1) {
+			Deltoid(size, size1);
+			Jobbra(-60);
+			Deltoid(size1, size);
+			Jobbra(60);
+            Előre(size1);
+            Jobbra(-60);
+            Előre(size);
+			Jobbra(120);
+			Deltoid(size, size);
+			Jobbra(-120);
+            Előre(-size);
+            Jobbra(60);
+            Előre(-size1);		
+		}
+		void RowSwap(double size) {
+			using(new Rajzol(false)) {
+                for (int i = 0; i < 2; i++) {
+                    Jobbra(60);
+                    Előre(size);
+					
+                }
+                Jobbra(-120);
+            }
+        }
+		void Row(double size, double size1,int length) {
+			for (int i = 0;i < length;i++) {
+				Brick(size, size1);
+				RowSwap(size);
+			}
+			for (int i = 0; i < length; i++) {
+				RowSwap(-size);
+			}
+        }
+		void PillarSwap(double size, double size1) {
+			Előre(size1);
+			Jobbra(60);
+			Előre(size);
+			Jobbra(-60);
+		}
+		void Piramis(double size, double size1, int length) {
+			for (int i = 0; i < length; i++) {
+				Row(size, size1, length - i);
+				PillarSwap(size, size1);
+			}
+			for (int i = 0; i < length; i++){
+				PillarSwap(-size, -size1);
+			}
+		}
+		/* Függvények vége */
+		void FELADAT()
+		{
+			/* Ezt indítja a START gomb! */
+			// Teleport(közép.X, közép.Y+150, észak);
+			Piramis(20, 32,6);
+			
+		}
+	}
+}
