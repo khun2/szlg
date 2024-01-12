f28331e51da2a0693e4405a91060388d13ae6437
e e <khunor22@gmail.com>
2024-01-12 08:30:22 +0100

    ads

---
+++ 23-24/kareszOOP/OOPK_/Diak.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Karesz
+{
+    public partial class Form1 : Form
+    {
+        void DIÁK_ROBOTJAI()
+        {
+            Robot karesz = Robot.Get("Karesz");
+
+            karesz.Feladat = delegate ()
+            {
+                //code goes here brrrrr
+            };
+        }
+    }
+}
