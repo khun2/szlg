82cfd319ab654c7dab8e0ceddca6425fd592bd95
khun <khunor22@gmail.com>
2024-01-12 16:03:46 +0100

    faaaaaaaaaaaaaaaaaaaaa

---
+++ 23-24/karesz/LKvastagodo/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LogoKaresz
+{
+	public partial class Form1 : Form
+	{
+		/* Függvények */
+		void Fa(int yrs, double size) {
+			if (yrs == 0) {return;}
+				using (new Vastagság(yrs + 1)) {
+			using (new Átmenetileg(Előre, size)) {
+					using (new Átmenetileg(Jobbra, -60))
+					{
+						Fa(yrs - 1, size / 3 * 2);
+					}
+					using (new Átmenetileg(Jobbra, 60))
+					{
+						Fa(yrs - 1, size / 3 * 2);
+					}
+				}
+			}
+		}
+
+		/* Függvények vége */
+		void FELADAT()
+		{
+			/* Ezt indítja a START gomb! */
+			// Teleport(közép.X, közép.Y+150, észak);
+
+			using(new Frissítés(false)) {
+                Fa(10, 100);
+            }
+		}
+	}
+}
