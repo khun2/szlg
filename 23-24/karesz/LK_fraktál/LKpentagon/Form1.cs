850b2ac6fb178867d5e72455a9babed15dd8ddb8
khun <khunor22@gmail.com>
2024-01-19 16:44:52 +0100

    asd

---
+++ 23-24/karesz/LK_fraktál/LKpentagon/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LogoKaresz {
+    public partial class Form1 : Form {
+        /* Függvények */
+        void Fra(int order, double size) {
+            if (order <= 1) {Előre(size); return; } 
+            Fra(order-1, size/2);
+            Jobbra(72);
+            Fra(order-1, size/2);
+            Jobbra(-144);
+            Fra(order-1, size/2);
+            using (new Átmenetileg(Jobbra, -72)) {
+                Fra(order - 1, size / 2);
+            }
+            Fra(order - 1, size / 2);
+            Jobbra(72);
+            Fra(order - 1, size / 2);
+        }
+        void Fra5(int order, double size) {
+            for (int i = 0; i < 5; i++) {
+                Fra(order, size);
+                Jobbra(72);
+            }
+        }
+        /* Függvények vége */
+        void FELADAT() {
+            /* Ezt indítja a START gomb! */
+            using (new Frissítés(false)) {
+                Teleport(közép.X / 1, közép.Y, észak);
+                Fra5(5, 50);
+            }
+        }
+    }
+}
--- 23-24/karesz/LK_fraktál/LKsierpinski/Form1.cs
+++ 23-24/karesz/LK_fraktál/LKsierpinski/Form1.cs
@@ -7,7 +7,7 @@ using System.Windows.Forms;
 namespace LogoKaresz {
     public partial class Form1 : Form {
         /* Függvények */
-        void Oldalaz(double size, double angle) {
+        /*void Oldalaz(double size, double angle) {
             using (new Rajzol(false))
             using (new Átmenetileg(Jobbra, angle)) {
                 Előre(size);
@@ -21,13 +21,29 @@ namespace LogoKaresz {
                 Nyíl(num-1, size/2);
             }
             Oldalaz(size, 0);
+        }*/
+        void Fra(double size, int rec) {
+            if (rec <= 1) { Előre(size); return; }
+            using (new Rajzol(false))
+                Balra(-30);
+            Fra(size / 2, rec - 1);
+            for (int i = 0; i < 2; i++) {
+                Jobbra(180);
+                Fra(size / 2, rec - 1);
+            }
+            Jobbra(60);
+            Fra(size / 2, rec - 1);
+            using (new Rajzol(false))
+                Jobbra(60);
+            Fra(size / 2, rec - 1);
+            Jobbra(210);
         }
         /* Függvények vége */
         void FELADAT() {
             /* Ezt indítja a START gomb! */
-            using (new Frissítés(false)) {
+            using (new Frissítés(true)) {
                 Teleport(közép.X / 1, közép.Y, észak);
-                Nyíl(2, 100);
+                Fra(40, 3);
             }
         }
     }
