3b227269d551fb68a4ce918a23c13222082390c5
khun <khunor22@gmail.com>
2024-01-19 18:02:32 +0100

    fraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktalfraktal

--- 23-24/karesz/LK_fraktál/LKcafrangos/Form1.cs
+++ 23-24/karesz/LK_fraktál/LKcafrangos/Form1.cs
@@ -3,50 +3,45 @@ using System.Drawing;
 using System.Threading;
 using System.Windows.Forms;
 
-namespace LogoKaresz
-{
-	public partial class Form1 : Form
-	{
-		/* Függvények */
-		void Fraktál(int yrs, double size, int sign=1) {
-			if (yrs <= 1) { Előre(size); return; }
-			Fraktál(yrs-1, size/3,sign);
-			using (new Átmenetileg(Jobbra, 60*sign))
-			{
-				Fraktál(yrs - 1, size / 3,sign);
-				using (new Átmenetileg(Jobbra, -120*sign))
-				{
-					Fraktál(yrs - 1, size / 3 ,sign);
-					Fraktál(yrs - 1, size / 3 , sign);
-				}
-				Fraktál(yrs-1, size/3, sign);
-			}   
-		}
-		void Szigetj(int szögszám, int yrs, double size) {
-			for (int i = 0; i < szögszám; i++) {
-				Fraktál(yrs, size);
-				Jobbra(360 / szögszám);
-			}
-		}
+namespace LogoKaresz {
+    public partial class Form1 : Form {
+        /* Függvények */
+        void Fraktál(int yrs, double size, int sign = 1) {
+            if (yrs <= 1) { Előre(size); return; }
+            Fraktál(yrs - 1, size / 3, sign);
+            using (new Átmenetileg(Jobbra, 60 * sign)) {
+                Fraktál(yrs - 1, size / 3, sign);
+                using (new Átmenetileg(Jobbra, -120 * sign)) {
+                    Fraktál(yrs - 1, size / 3, sign);
+                    Fraktál(yrs - 1, size / 3, sign);
+                }
+                Fraktál(yrs - 1, size / 3, sign);
+            }
+        }
+        void Szigetj(int szögszám, int yrs, double size) {
+            for (int i = 0; i < szögszám; i++) {
+                Fraktál(yrs, size);
+                Jobbra(360 / szögszám);
+            }
+        }
         void Szigete(int szögszám, int yrs, double size) {
 
             for (int i = 0; i < szögszám; i++) {
                 Fraktál(yrs, size);
-				Jobbra(-360 / szögszám);
+                Jobbra(-360 / szögszám);
             }
 
         }
         /* Függvények vége */
         void FELADAT() {
-			/* Ezt indítja a START gomb! */
-			using (new Frissítés(false)) {
-				Teleport(közép.X , közép.Y, észak);
-				using (new Átmenetileg(Jobbra, 0)) {
-					//Fraktál(2, 100,1);
-					Szigete(6,6,100);
-				}
-
-			}
-		}
-	}
+            /* Ezt indítja a START gomb! */
+            using (new Frissítés(false)) {
+                Teleport(közép.X, közép.Y, észak);
+                using (new Átmenetileg(Jobbra, 0)) {
+                    //Fraktál(2, 100,1);
+                    Szigete(6, 6, 100);
+                }
+            }
+        }
+    }
 }
--- 23-24/karesz/LK_fraktál/LKsierpinski/Form1.cs
+++ 23-24/karesz/LK_fraktál/LKsierpinski/Form1.cs
@@ -7,43 +7,39 @@ using System.Windows.Forms;
 namespace LogoKaresz {
     public partial class Form1 : Form {
         /* Függvények */
-        /*void Oldalaz(double size, double angle) {
-            using (new Rajzol(false))
-            using (new Átmenetileg(Jobbra, angle)) {
-                Előre(size);
-}
-        }
-        void Nyíl(int num, double size) {
-            if (num == 1) { Jobbra(90); Előre(size); Jobbra(-90); return; }
-            Jobbra(30);
-            Oldalaz(size, 0);
-            using(new Átmenetileg(Jobbra, 90)) {
-                Nyíl(num-1, size/2);
+        void Nyíl(double size, int rec) {
+            if (rec <= 1) { Jobbra(90); Előre(size);Jobbra(-90); return; }
+            using (new Rajzol(false)) {
+                Jobbra(-60);
+                Nyíl(size / 2, rec - 1);
+                Jobbra(-180);
             }
-            Oldalaz(size, 0);
-        }*/
-        void Fra(double size, int rec) {
-            if (rec <= 1) { Előre(size); return; }
-            using (new Rajzol(false))
-                Balra(-30);
-            Fra(size / 2, rec - 1);
-            for (int i = 0; i < 2; i++) {
+            Nyíl(size / 2, rec - 1);
+            using (new Rajzol(false)) {
                 Jobbra(180);
-                Fra(size / 2, rec - 1);
-            }
-            Jobbra(60);
-            Fra(size / 2, rec - 1);
-            using (new Rajzol(false))
+                Nyíl(size / 2, rec - 1);
                 Jobbra(60);
-            Fra(size / 2, rec - 1);
-            Jobbra(210);
+            }
+            Nyíl(size / 2, rec - 1);
+            using (new Rajzol(false)) {
+                Jobbra(60);
+                Nyíl(size / 2, rec - 1);
+                Jobbra(-180);
+            }
+            Nyíl(size / 2, rec - 1);
+            using (new Rajzol(false)) {
+                Jobbra(180);
+                Nyíl(size / 2, rec - 1);
+                Jobbra(-60);
+            }
         }
         /* Függvények vége */
         void FELADAT() {
             /* Ezt indítja a START gomb! */
-            using (new Frissítés(true)) {
-                Teleport(közép.X / 1, közép.Y, észak);
-                Fra(40, 3);
+            using (new Frissítés(false)) {
+                Teleport(közép.X / 2, közép.Y+150, észak);
+                //ez a megoldás nagyon ineficcient és lassú de működik
+                Nyíl(300, 7);
             }
         }
     }
