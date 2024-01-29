a567e3c74a11181cbc42fbb5dcd45b94fb769fa0
e e <khunor22@gmail.com>
2024-01-29 14:11:43 +0100

    asdf

---
+++ 23-24/kareszOOP/OOPK_paththing/Diak.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace Karesz {
+    public partial class Form1 : Form {
+        void DIÁK_ROBOTJAI() {
+            Robot karesz = Robot.Get("Karesz");
+
+            karesz.Feladat = delegate () {
+                void _06_30() {
+                    Jobbra(2);
+                    while (!Van_e_előttem_fal()) {
+                        Előre();
+                    }
+                    Jobbra();
+                    while (!Van_e_előttem_fal()) {
+                        Előre();
+                    }
+                    Jobbra();
+                    while (true) {
+                        if (Kilépek_e_a_pályáról()) {
+                            break;
+                        }
+                        if (!Van_e_előttem_fal()) {
+                            Előre();
+                        }
+                        Jobbra();
+                        if (!Van_e_előttem_fal()) {
+                            Előre();
+                        }
+                        else {
+                            Balra();
+                        }
+                        if (Van_e_előttem_fal()) {
+                            Balra();
+                        }
+                    }
+                }
+                _06_30();
+            };
+        }
+    }
+}
