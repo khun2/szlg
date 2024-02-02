80da3c45da0d242839930c9212eaea4bfddc84b1
e e <khunor22@gmail.com>
2024-02-02 08:39:56 +0100

    asd

--- 23-24/kareszOOP/OOPK_31/Diak.cs
+++ 23-24/kareszOOP/OOPK_31/Diak.cs
@@ -12,9 +12,16 @@ namespace Karesz {
     public partial class Form1 : Form {
         void DIÁK_ROBOTJAI() {
             Robot karesz = Robot.Get("Karesz");
-            void Elomozog(bool i=true) {
+            void Elomozog(bool i = true) {
                 if (!Van_e_előttem_fal()) {
                     Előre();
+                    if (Mi_van_alattam() == piros) {
+                        return;
+                    }
+                    else if (Van_e_itt_Kavics()) {
+                        Vegyél_fel_egy_kavicsot();
+
+                    }
                     return;
                 }
                 else if (i) {
@@ -32,15 +39,22 @@ namespace Karesz {
                 }
                 Tegyél_le_egy_kavicsot(piros);
                 Elomozog();
-                while (true) {
+                while (Mi_van_alattam() != piros) {
                     while (!Van_e_előttem_fal()) {
                         Előre();
-                        if (Van_e_itt_Kavics()) {
+                        if (Mi_van_alattam() == piros) {
+                            break;
+                        }
+                        else if (Van_e_itt_Kavics()) {
                             Vegyél_fel_egy_kavicsot();
                         }
                     }
-                    Jobbra();
+                    if (Mi_van_alattam() == piros) {
+                        break;
+                    }
+                    Elomozog();
                 }
+                Vegyél_fel_egy_kavicsot();
             }
             karesz.Feladat = delegate () {
                 //code goes here brrrrr 
