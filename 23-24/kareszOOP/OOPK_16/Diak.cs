6da85a11290a2e263a238f1d4652c6ffab3e3986
e e <khunor22@gmail.com>
2024-02-05 14:11:06 +0100

    asd

--- 23-24/kareszOOP/OOPK_16/Diak.cs
+++ 23-24/kareszOOP/OOPK_16/Diak.cs
@@ -12,37 +12,42 @@ namespace Karesz {
     public partial class Form1 : Form {
         void DIÁK_ROBOTJAI() {
             Robot karesz = Robot.Get("Karesz");
-            void Keres(bool i = true) {
-                if (!Van_e_előttem_fal()) {
-                    if (Van_e_itt_Kavics()) {
-                        Vegyél_fel_egy_kavicsot();
+            void Keres(int count = 0, bool i = true) {
+                while (!Kilépek_e_a_pályáról()) {
+                if (count >= 7) { break; }
+                    Console.WriteLine(count);
+                    count++;
+                    if (!Van_e_előttem_fal()) {
+                        if (Van_e_itt_Kavics()) {
+                            count = 0;
+                            Vegyél_fel_egy_kavicsot();
+                        }
+                        else { Jobbra(); }
+                        if (!Van_e_előttem_fal()) {
+                            Előre();
+                        }
                     }
                     else { Jobbra(); }
-                    if (!Van_e_előttem_fal()) {
-                        Előre();
+                    if (!Van_e_itt_Kavics()) {
+                        if (i) {
+                            Jobbra();
+                            Keres(count, false);
+                        }
+                        else {
+                            Jobbra(2);
+                            Keres(count);
+                        }
                     }
-                }
-                else { Jobbra(); }
-                if (!Van_e_itt_Kavics()) {
-                    if (i) {
-                        Jobbra();
-                        Keres(false);
-                    }
-                    else {
-                        Jobbra(2);
-                        Keres();
-                    }
+                    Keres(count);
                 }
             }
 
             void _16() {
-                while (!Kilépek_e_a_pályáról()) {
-                    Keres();
-                }
+
             }
             karesz.Feladat = delegate () {
                 //code goes here brrrrr 
-                _16();
+                Keres();
 
             };
         }
