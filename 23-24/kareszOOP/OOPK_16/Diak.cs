f0e2ca4e0e5ef0060df2d63d45690a81cf4c74f2
e e <khunor22@gmail.com>
2024-02-08 13:06:53 +0100

    asd

--- 23-24/kareszOOP/OOPK_16/Diak.cs
+++ 23-24/kareszOOP/OOPK_16/Diak.cs
@@ -12,43 +12,50 @@ namespace Karesz {
     public partial class Form1 : Form {
         void DIÁK_ROBOTJAI() {
             Robot karesz = Robot.Get("Karesz");
-            void Keres(int count = 0, bool i = true) {
-                while (!Kilépek_e_a_pályáról()) {
-                if (count >= 7) { break; }
-                    Console.WriteLine(count);
-                    count++;
-                    if (!Van_e_előttem_fal()) {
-                        if (Van_e_itt_Kavics()) {
-                            count = 0;
-                            Vegyél_fel_egy_kavicsot();
-                        }
-                        else { Jobbra(); }
-                        if (!Van_e_előttem_fal()) {
-                            Előre();
-                        }
-                    }
-                    else { Jobbra(); }
+            void Keres(bool i = true) {
+                if (i) {
+                    Jobbra();
+                }
+                else {
+                    Balra();
+                }
+                if (!Van_e_előttem_fal()) {
+                    Előre();
                     if (!Van_e_itt_Kavics()) {
-                        if (i) {
-                            Jobbra();
-                            Keres(count, false);
-                        }
-                        else {
-                            Jobbra(2);
-                            Keres(count);
-                        }
+                        Jobbra(2);
+                        Előre();
+                        Balra(1);
+                        Keres(!i);
                     }
-                    Keres(count);
+                    else { return; }
+                }
+                else {
+                    if (i) {
+                        Balra();
+                    }
+                    else {
+                        Jobbra();
+                    }
+                    Keres(!i);
                 }
             }
-
             void _16() {
-
+                while (!Kilépek_e_a_pályáról()) {
+                    while (!Van_e_előttem_fal()) {   
+                        Előre();
+                        if (!Van_e_itt_Kavics()) {
+                            Jobbra(2);
+                            Előre();
+                            Jobbra(2);
+                            break;
+                        }
+                    }
+                    Keres();
+                }
             }
             karesz.Feladat = delegate () {
-                //code goes here brrrrr 
-                Keres();
-
+                _16();
+                //Keres();
             };
         }
     }
