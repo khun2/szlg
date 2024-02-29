3cb42cec1e6513b49cf34664971973dbff2c27d9
e e <khunor22@gmail.com>
2024-02-29 12:56:07 +0100

    asfd

---
+++ 23-24/kareszOOP/OOPK_mindenmásodik/Diak.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Karesz
+{
+    public partial class Form1 : Form
+    {
+        void BalLe() {
+            for (int i = 0; i < 2; i++) {
+                Balra(1);
+                while (!Van_e_előttem_fal()) {
+                    Előre();
+                }
+            }
+            Jobbra(2);
+        }
+        bool checkWall() {
+            Balra();
+            if (!Van_e_előttem_fal()) {
+                return true;
+            }
+            Jobbra();
+            return false;
+        }
+        Random random_generator = new Random();
+        void Falkövetés() {
+            BalLe();
+            int asd = 1;
+            while (true) {
+                if (checkWall()) {
+                    break;
+                }
+                if (asd==1) {
+                    int rand1 = random_generator.Next(2, 7);
+                    Tegyél_le_egy_kavicsot(rand1);
+                }
+                Előre();
+                asd *= -1;
+                if (Van_e_előttem_fal()) {
+                    if (checkWall()) {
+                        if (asd == 1) {
+                            int rand1 = random_generator.Next(2, 7);
+                            Tegyél_le_egy_kavicsot(rand1);
+                        }
+                        break;
+                    }
+                    Jobbra();
+                }
+            }
+                        Előre();
+        }
+        void DIÁK_ROBOTJAI()
+        {
+            Robot karesz = Robot.Get("Karesz");
+
+            karesz.Feladat = delegate ()
+            {
+                //code goes here brrrrr 
+                Falkövetés();
+            };
+        }
+    }
+}
---
+++ 23-24/kareszOOP/OOPK_mindenmásodik/Tanar.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace Karesz
+{
+    public partial class Form1 : Form
+    {
+        string betöltendő_pálya = "palya04.txt";
+
+        void TANÁR_ROBOTJAI()
+        {
+            int feketedb = 100;
+            int pirosdb = 100; 
+            int zölddb = 100;
+            int sárgadb = 100;
+            int hódb = 100;
+
+            int pos_x = 5;
+            int pos_y = 28;
+
+            new Robot("Karesz", feketedb, pirosdb, zölddb, sárgadb, hódb, pos_x, pos_y, 0);
+
+            /*
+            Robot janesz = new Robot("Janesz", Robot.képkészlet_lilesz, 0, 0, 0, 0, 0, 14, 1, 2);
+
+            janesz.Feladat = delegate ()
+            {
+                //
+            };
+
+            Robot fanesz = new Robot("Fanesz", Robot.képkészlet_lilesz, 0, 0, 0, 0, 0, 16, 1, 2);
+
+            fanesz.Feladat = delegate ()
+            {
+                //
+            };
+            */
+
+        }
+    }
+}
