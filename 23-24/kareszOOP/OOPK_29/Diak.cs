2fdda7ff298caba7e518ee03a0a8c36cbda9dd8a
khun2 <khunor22@gmail.com>
2024-03-03 19:14:42 +0100

    commit deez nuts

--- 23-24/kareszOOP/OOPK_29/Diak.cs
+++ 23-24/kareszOOP/OOPK_29/Diak.cs
@@ -48,12 +48,10 @@ namespace Karesz {
                         else {
                             if (Mi_van_alattam()==3) {
                                 Vegyél_fel_egy_kavicsot();
-                                Tegyél_le_egy_kavicsot();
                                 pkő++;
                             }
                             else if(Mi_van_alattam() == 5) {
                                 Vegyél_fel_egy_kavicsot();
-                                Tegyél_le_egy_kavicsot();
                                 skő++;
                             }
                             Előre();
@@ -64,13 +62,47 @@ namespace Karesz {
                     else {
                         if (Mi_van_alattam() == 3) {
                             Vegyél_fel_egy_kavicsot();
-                            Tegyél_le_egy_kavicsot();
                             pkő++;
                         }
                         else if (Mi_van_alattam() == 5) {
                             Vegyél_fel_egy_kavicsot();
-                            Tegyél_le_egy_kavicsot();
                             skő++;
+                        }
+                        Előre();
+                    }
+                }
+                KFordulj(-1);
+            }
+            void VégigRak() {
+                int sign = 1;
+                while (true) {
+                    if (Kilépek_e_a_pályáról()) {
+                        KFordulj(sign);
+                        if (Kilépek_e_a_pályáról()) {
+                            break;
+                        }
+                        else {
+                            if (pkő > 0 && !Van_e_itt_Kavics()){
+                                Tegyél_le_egy_kavicsot(3);
+                                pkő--;
+                            }
+                            if (skő > 0 && !Van_e_itt_Kavics()){
+                                Tegyél_le_egy_kavicsot(5);
+                                skő--;
+                            }
+                            Előre();
+                            KFordulj(sign);
+                            sign *= -1;
+                        }
+                    }
+                    else {
+                        if (pkő > 0 && !Van_e_itt_Kavics()) {
+                            Tegyél_le_egy_kavicsot(3);
+                            pkő--;
+                        }
+                        if (skő > 0 && !Van_e_itt_Kavics()) {
+                            Tegyél_le_egy_kavicsot(5);
+                            skő--;
                         }
                         Előre();
                     }
@@ -80,8 +112,12 @@ namespace Karesz {
             void _28() {
                 BalLe();
                 Végigmegy();
-                MessageBox.Show($"{pkő}\n{skő}");
-                BalLe();
+                Jobbra(2);
+                while (!Kilépek_e_a_pályáról()) {
+                    Előre();
+                }
+                Jobbra();
+                VégigRak();
             }
 
 
