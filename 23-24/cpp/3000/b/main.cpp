2ed4991764adb94907eba3e054db8a79a75a44d5
khun2 <khunor22@gmail.com>
2024-03-05 07:30:58 +0100

    2024. márc. 5., kedd, 07:30:58 CET

--- 23-24/cpp/3000/b/main.cpp
+++ 23-24/cpp/3000/b/main.cpp
@@ -33,9 +33,39 @@ int feladat4(const vector<int>vec){
     return i;
 }
 int feladat5(const vector<int>vec){
-
+    int num=0;
+    for(int i: vec){
+        num+=i;
+    }
+    return (num/vec.size())/2;
 }
-
+string feladat6(const vector <int> vec){
+    for(int i:vec){
+        if(i<0){
+            return "NO";
+        }
+    }
+    return "YES";
+}
+int feladat7(const vector <int> vec){
+    int num=0;
+    for(int i: vec){
+        if(i%2==1){
+            num+=1;
+        }
+    }
+    return num;
+}
+string feladat8(const vector <int> vec){
+    for (size_t i = 1; i < vec.size(); i++)
+    {
+        if(vec[i-1]<0&&vec[i]<0){
+            return "YES";
+        }
+    }
+    return "NO";
+    
+}
 int main() {
     vector<int> vec;
 
