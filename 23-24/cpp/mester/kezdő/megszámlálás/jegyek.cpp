b3ac4867f18e30ee16721828901fb020681f38ff
khun2 <khunor22@gmail.com>
2024-03-08 18:49:37 +0100

    2024. márc. 8., péntek, 18:49:37 CET

--- 23-24/cpp/mester/kezdő/megszámlálás/jegyek.cpp
+++ 23-24/cpp/mester/kezdő/megszámlálás/jegyek.cpp
@@ -3,31 +3,16 @@ using namespace std;
 
 int main()
 {
-    int num;
-    array<int,6> arr;
-    arr.fill(0);
-    cin>>arr[0];
-    for (int i = 0; i < arr[0]; i++)
+    array<int,5> arr;
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++)
     {
+        int num;
         cin >> num;
-        switch (num)
-        {
-        case 1:
-            arr[1] += 1;
-            break;
-        case 2:
-            arr[2] += 1;
-            break;
-        case 3:
-            arr[3] += 1;
-            break;
-        case 4:
-            arr[4] += 1;
-            break;
-        case 5:
-            arr[5] += 1;
-            break;
-        }
+        arr[num-1]++;
     }
-    cout << arr[1] << " " << arr[2] << " " << arr[3] << " " << arr[4] << " " << arr[5];
+    for(int x : arr) {
+        cout << x << ' '; 
+    }
 }
