919d1f8e80a6ea1639c15ee6dc7e39f0cd01f5e3
khun2 <khunor22@gmail.com>
2024-03-14 19:36:40 +0100

    2024. márc. 14., csütörtök, 19:36:40 CET

--- 23-24/cpp/mester/kezdő/sorozatszámítás/Első teaforgalmazó összes eladott mennyisége.cpp
+++ 23-24/cpp/mester/kezdő/sorozatszámítás/Első teaforgalmazó összes eladott mennyisége.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main(){
-    
+    int n, out, num;
+    string f, s;
+    cin >> n >> f >> out;
+    for (int i = 0; i < n-1; i++) {
+        cin >> s >> num;
+        if(s == f) {
+            out += num;
+        }
+    }
+    cout<<out;
 }
