f138fdd3d24c212408de9d9d3c9d69eec52ab4a2
khun2 <khunor22@gmail.com>
2024-03-28 21:12:07 +0100

    fix d

--- 23-24/cpp/3000/d/main.cpp
+++ 23-24/cpp/3000/d/main.cpp
@@ -1,26 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int feladat1(const vector<int>& v) {
-    int num=v[0];
+long long feladat1(const vector<int>& v) {
+    long long num=v[0];
     for(int i=1;i<v.size();i++){
         num*=v[i];
     }
     return num;
 }
 int feladat2(const vector<int>& v) {
-    for (int i = v.size() - 1; i >= 0; i--)
-    {
-        if(v[i]%5==0||v[i]%7==0){
+    //lazy vagyok átírni ezeket while loopba
+    for (int i = v.size() - 1; i >= 0; i--) {
+        if(v[i] % 5 == 0 || v[i] % 7 == 0) {
             return i;
         }
     }
     return -1;
 }
 int feladat3(const vector<int>& v) {
-    for (int i = 0; i < v.size(); i++)
-    {
-        if(v[i]%21==0){
+    for (int i = 0; i < v.size(); i++) {
+        if(v[i] % 21 == 0) {
             return i;
         }
     }
@@ -28,22 +27,16 @@ int feladat3(const vector<int>& v) {
 }
 bool feladat4(const vector<int>& v) {
     int i = 0;
-    while(v[i] >= 0 && i <= v.size()) {
+    while(i < v.size() && v[i] < 0) {
         i++;
     }
-    return i != v.size();
+    return i == v.size();
 }
 bool feladat5(const vector<int>& v) {
-    /*for(int i:v){
-        if(i>0&&i<10){
-            return "YES";
-        }
-    }*/
     int i = 0;
-    while ((v[i] < 1 || v[i] > 10) && i <= v.size()) {
+    while (i < v.size() && (v[i] < 1 || v[i] > 10)) {
         i++;
     }
-    
     return i != v.size();
 }
 int feladat6(const vector<int>& v) {
@@ -56,17 +49,15 @@ int feladat6(const vector<int>& v) {
     return num;
 }
 int feladat7(const vector<int>& v) {
-    int smal=v[0], out=0;
-    for(int i=1;i<v.size();i++){
-        if(v[i]<smal){
-            smal=v[i];
-            out=v[i];
+    int out=0;
+    for(int i = 1;i < v.size(); i++) {
+        if(v[i] < v[out]){
+            out = i;
         }
     }
     return out;
 }
 vector<int> feladat8(const vector<int>& v) {
-    cout<<"feladat 8:\n";
     vector<int> out;
     for(int i:v){
         if(i % 17==0 || i % 18 == 0){
@@ -79,8 +70,8 @@ int feladat9(const vector<int>& v) {
     return v.size();
 }
 bool feladat10(const vector<int>& v) {
-    int i = 1;
-    while((v[i] <= 0 && v[i-1] <= 0 && v[i+1] >= 0) && i <= v.size()) {i++; }
+    int i = 2;
+    while(i < v.size() && (v[i-2] <= 0 && v[i-1] >= 0 && v[i] <= 0)) {i++; }
     return i != v.size();
 }
 vector <pair<int, bool>> feladat11(const vector<int>& v) {
@@ -111,11 +102,12 @@ int main() {
     cout << "feladat 5: " << feladat5(input) << '\n';
     cout << "feladat 6: " << feladat6(input) << '\n';
     cout << "feladat 7: " << feladat7(input) << '\n';
+    cout<<"feladat 8:\n";
     vector<int> f8 = feladat8(input);
     for(int x : f8){
         cout<<x<<' ';
     }
-    cout << "feladat 9: " << feladat9(input) << '\n';
+    cout << "\nfeladat 9: " << feladat9(input) << '\n';
     cout << "feladat 10: " << feladat10(input) << '\n';
     cout << "feladat 11:\n";
     vector<pair<int, bool>> f11 = feladat11(input);
