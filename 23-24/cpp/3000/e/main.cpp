a744c23ac99fd7fc914f8c04d50043df7d9ab641
khun2 <khunor22@gmail.com>
2024-03-28 21:22:07 +0100

    fix e

--- 23-24/cpp/3000/e/main.cpp
+++ 23-24/cpp/3000/e/main.cpp
@@ -7,19 +7,19 @@ int feladat1(const vector<int>& v) {
 }
 int feladat2(const vector<int>& v) {
     int i=v.size()-1;
-    while (v[i]%3==0 && i >= -1) {
+    while (i > -1 && v[i]%3 != 0) {
         i--;
     }
     return i;
 }
 int feladat3(const vector<int>& v) {
     int i = 0;
-    while (v[i] % 15 == 0 && i <= v.size()) {i++;}
+    while (i < v.size() && v[i] % 15 != 0) {i++;}
     return i == v.size() ? -1 : i;
 }
 bool feladat4(const vector<int>& v) {
     int i = 0;
-    while ((v[i] < -10 || v[i] > 10) && i <= v.size()) {i++;}
+    while (i < v.size() && (v[i] >= -10 && v[i] <= 10)) {i++;}
     return i == v.size();
 }
 long long feladat5(const vector<int>& v) {
@@ -29,17 +29,17 @@ long long feladat5(const vector<int>& v) {
         num *= x;
         num %= mod;
     }
-    return num*2;
+    return (num*2) % mod;
 }
 int feladat6(const vector<int>& v) {
     int num = 0;
     for(int x : v) {
-        if(x % 108 == 0) {num++;}
+        //őszintén én sem tudom mit szívtam amikor elsőnek írtam
+        if(x % 18 == 0) {num++;}
     }
     return num;
 }
 vector <int> feladat7(const vector<int>& v) {
-    cout<<"feladat 7:\n";
     vector <int> out;
     for(int x : v) {
         if(x % 17 == 0 || x % 18 == 0) {
@@ -65,10 +65,10 @@ int feladat8(const vector<int>& v) {
 }
 bool feladat9(const vector<int>& v) {
     int i = 0;
-    while (sqrt(v[i]) - floor(sqrt(v[i])) != 0 && i < v.size()) {
+    while (i < v.size() && sqrt(v[i]) - floor(sqrt(v[i])) != 0) {
         i++;
     }
-    return i!=v.size();
+    return i != v.size();
 }
 bool feladat10(const vector<int>& v) {
     for (size_t i = 1; i < v.size()-1; i++)
@@ -106,12 +106,12 @@ int main() {
     cout << "feladat 4: " << feladat4(input) << '\n';
     cout << "feladat 5: " << feladat5(input) << '\n';
     cout << "feladat 6: " << feladat6(input) << '\n';
+    cout<<"feladat 7:\n";
     vector <int> f7 =feladat7(input);
     for(int x : f7) {
         cout<<x<<" ";
     }
-    cout<<'\n';
-    cout << "feladat 8: " << feladat8(input) << '\n';
+    cout << "\nfeladat 8: " << feladat8(input) << '\n';
     cout << "feladat 9: " << feladat9(input) << '\n';
     cout << "feladat 10: " << feladat10(input) << '\n';
     cout << "feladat 11:\n";
