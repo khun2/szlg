8bc5e45e27c3b214f70826fa1cfb533a7f4590ec
khun2 <khunor22@gmail.com>
2024-04-26 16:01:19 +0200

    2024. ápr. 26., péntek, 16:01:19 CEST

---
+++ 23-24/cpp/3000/j/main.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace osztalyok_23f
+{
+    internal class Program
+    {
+        class Tanulo
+        {
+            public int tanulokod;
+            public string nev;
+            public string matinfo;
+            public string angol;
+            public string nyelv2;
+            public string nem;
+            public int egyuttlakoszam;
+            public int testverszam;
+            public Tanulo(int tanulokod, string nev, string matinfo, string angol, string nyelv2, string nem, int egyuttlakoszam, int testverszam)
+            {
+                this.tanulokod = tanulokod;
+                this.nev = nev;
+                this.matinfo = matinfo;
+                this.angol = angol;
+                this.nyelv2 = nyelv2;
+                this.nem = nem;
+                this.egyuttlakoszam = egyuttlakoszam;
+                this.testverszam = testverszam;
+            }
+        }
+        static List<Tanulo> Beolvas(string fajlnev)
+        {
+            List<Tanulo> lista = new List<Tanulo>();
+
+            string[] sorok = File.ReadAllLines(fajlnev);
+            foreach (string sor in sorok)
+            {
+                string[] sortomb = sor.Split(';');
+                Tanulo tanulo = new Tanulo(int.Parse(sortomb[0]), sortomb[1], sortomb[2], sortomb[3], sortomb[4], sortomb[5], int.Parse(sortomb[6]), int.Parse(sortomb[7]));
+                lista.Add(tanulo);
+            }
+            return lista;
+        }
+        static int Adott_nemu_tanulok_szama(List<Tanulo> lista, string nem)
+        {
+            int result = 0;
+            foreach (Tanulo tanulo in lista)
+            {
+                if (tanulo.nem == nem)
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+        static int TöbbMintxTestvérNum(List<Tanulo> l, int x)
+        {
+            int result = 0;
+            foreach (Tanulo tanulo in l)
+            {
+                if (tanulo.testverszam > x)
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+        static void TöbbMintxTestvérList(List<Tanulo> l, int x)
+        {
+               
+            foreach (Tanulo tanulo in l)
+            {
+                if (tanulo.testverszam > x)
+                {
+
+                }
+            }
+
+        }
+        
+        static void Main(string[] args)
+        {
+            List<Tanulo> lista = Beolvas("input.txt");
+            Console.WriteLine("1. Hány diák tanul az osztályban?");
+            Console.WriteLine(lista.Count);
+            Console.WriteLine("2. Hány fiú tanul az osztályban?");
+            Console.WriteLine(Adott_nemu_tanulok_szama(lista, "F"));
+            Console.WriteLine("3. Hány lány tanul az osztályban?");
+            Console.WriteLine(Adott_nemu_tanulok_szama(lista, "L"));
+            Console.WriteLine("Hány olyan diák van, akiknek több mint 1 testvére van?");
+            System.Console.WriteLine();
+        }
+    }
+}
