ecc92dc79143c483b49b84c3096394d0dd19d128
khun2 <khunor22@gmail.com>
2024-06-05 18:01:32 +0200

    real bottom of the barrel code in here

--- 23-24/cs/Meteorologiai jelentes/Program.cs
+++ 23-24/cs/Meteorologiai jelentes/Program.cs
@@ -16,15 +16,22 @@ namespace MeteorologiaiJelentes {
             public int temp;
             public Jelentes(string[] args) {
                 code = args[0];
-                time = args[1];
+                StringBuilder sb = new StringBuilder();
+                sb.Append(args[1][0]);
+                sb.Append(args[1][1]);
+                sb.Append(':');
+                sb.Append(args[1][2]);
+                sb.Append(args[1][3]);
+
+                time = sb.ToString();
                 angleSpeed = args[2];
                 temp = int.Parse(args[3]);
             }
             public string Feladat3() {
-                return $"név: {code}, időpont: {int.Parse(time)/100}:{int.Parse(time)%100}, hőmérséklet: {temp}";
+                return $"{code} {time} {temp} fok";
             }
             public string Feladat4() {
-                return $"név: {code}, időpont: {int.Parse(time)/100}:{int.Parse(time)%100}";
+                return $"{code} {time}";
             }
         }
 
@@ -39,7 +46,26 @@ namespace MeteorologiaiJelentes {
                 list.Add("Nem volt szélcsen a mérések idején");
             return list;
         }
-    
+
+        static List<string> f5(Jelentes[] input ) {
+            List<string> strings= new List<string>();
+            var groups = input.GroupBy(x => x.code);
+            foreach(var group in groups) {
+                StringBuilder sb = new StringBuilder(group.Key);
+                if(group.Any(x => x.time[0] == '0' && x.time[1] == '1') && group.Any(x => x.time[0] == '0' && x.time[1] == '7') && group.Any(x => x.time[0] == '1' && x.time[1] == '3') && group.Any(x => x.time[0] == '1' && x.time[1] == '9')) {
+                    
+                    sb.Append($" Középhőmérséklet: {Math.Round(group.Where(x => (x.time[0] == '0' && x.time[1] == '1') ||
+                    (x.time[0] == '0' && x.time[1] == '7') || (x.time[0] == '1' && x.time[1] == '3') ||
+                    (x.time[0] == '1' && x.time[1] == '9')).Average(x => x.temp))}");
+                    //god is dead and my horrid code killed him
+                }
+                else sb.Append(" NA");
+                sb.Append($"; Hőmérséklet-ingadozás: {group.Max(x => x.temp) - group.Min(x => x.temp)}");
+                strings.Add(sb.ToString());
+            }
+            return strings;
+        }
+
         static void f6 (Jelentes[] input) {
             foreach(var group in input.GroupBy(x => x.code)) {
                 using (StreamWriter writer = new StreamWriter($"{group.Key}.txt")){
@@ -59,13 +85,14 @@ namespace MeteorologiaiJelentes {
 
         static void Main(string[] args) {
             Jelentes[] input = File.ReadAllLines("tavirathu13.txt").Select(x => new Jelentes(x.Split(' '))).ToArray();
-            //string str = Console.ReadLine();
-            //Console.WriteLine("2. feladat: " + (input.Where(x => x.code == str).Max(x => int.Parse(x.time))/100) + ":" + input.Where(x => x.code == str).Max(x => int.Parse(x.time))%100);
-            Console.WriteLine("3. feladat:\n" + input.MinBy(x => x.temp).Feladat3() + '\n' + input.MaxBy(x => x.temp).Feladat3());
+            string str = Console.ReadLine();
+            Console.WriteLine("2. feladat: " + input.Where(x => x.code == str).Max(x => x.time));
+            Console.WriteLine("3. feladat:\nA legalacsonyabb hőmérséklet: " + input.MinBy(x => x.temp).Feladat3() + "\nA legmagasabb hőmérséklet: " + input.MaxBy(x => x.temp).Feladat3());
             Console.WriteLine($"4. feladat");
             f4(input).ForEach(Console.WriteLine);
+            f5(input).ForEach(Console.WriteLine);
             f6(input);
-
+            
         }
     }
 }
