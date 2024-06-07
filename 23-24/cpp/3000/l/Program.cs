c6799c9ec276f1eeb9dad84900bb2b9ad1a4480e
khun2 <khunor22@gmail.com>
2024-06-07 22:04:02 +0200

    more fixes in l

--- 23-24/cpp/3000/l/Program.cs
+++ 23-24/cpp/3000/l/Program.cs
@@ -22,7 +22,7 @@ namespace l
                 int[] dt = birth.Split('.').Select(x => int.Parse(x)).ToArray();
                 this.name = name;
                 this.gender = int.Parse(gender);
-                this.birth = new DateTime(dt[0], dt[1], dt[3]);
+                this.birth = new DateTime(dt[0], dt[1], dt[2]);
                 this.city = city;
                 this.country = country;
                 this.films = int.Parse(films);
@@ -109,54 +109,54 @@ namespace l
             }
             Console.WriteLine($"5. Írja ki a Tokióban született színészek nevét és filmjeinek a számát!");
             
-            foreach (Actor actor in CityChooser(input, "Toki"))
+            foreach (Actor actor in CityChooser(input, "Tokió"))
             {
                 Console.WriteLine(actor.name + actor.films);
             }
 
-            Console.WriteLine($"6. Hány színész született Budapesten?\n{input.Where(x => x.city == "Budapest").Count()}");
-            Console.WriteLine($"7. Hány színész született Berlinben?\n{input.Where(x => x.city == "Berlint").Count()}");
-            Console.WriteLine($"8. Hány színész született New Yorkban?\n{input.Where(x => x.city == "New York").Count()}");
+            Console.WriteLine($"6. Hány színész született Budapesten?\n{input.Count(x => x.city == "Budapest")}");
+            Console.WriteLine($"7. Hány színész született Berlinben?\n{input.Count(x => x.city == "Berlint")}");
+            Console.WriteLine($"8. Hány színész született New Yorkban?\n{input.Count(x => x.city == "New York")}");
             
-            Console.WriteLine($"9. Hány színész született USA-ban?\n{input.Where(x => x.country == "USA")}");
-            Console.WriteLine($"10. Hány színész született Magyarországon?\n{input.Where(x => x.country == "Magyarország")}");
-            Console.WriteLine($"11. Hány színész született Németországban?\n{input.Where(x => x.country == "Németország")}");
-            Console.WriteLine($"12. Hány színész született Mexikóban?\n{input.Where(x => x.country == "Mexikó")}");
-            Console.WriteLine($"13. Hány színész született az USA-ban?\n{input.Where(x => x.country == "USA")}");
+            Console.WriteLine($"9. Hány színész született USA-ban?\n{input.Count(x => x.country == "USA")}");
+            Console.WriteLine($"10. Hány színész született Magyarországon?\n{input.Count(x => x.country == "Magyarország")}");
+            Console.WriteLine($"11. Hány színész született Németországban?\n{input.Count(x => x.country == "Németország")}");
+            Console.WriteLine($"12. Hány színész született Mexikóban?\n{input.Count(x => x.country == "Mexikó")}");
+            Console.WriteLine($"13. Hány színész született az USA-ban?\n{input.Count(x => x.country == "USA")}");
             
-            Console.WriteLine($"14. Hány férfi és hány női színész van az input fájlban?\n{input.Where(x => x.gender == 1)}, {input.Where(x => x.gender == 0)}");
+            Console.WriteLine($"14. Hány férfi és hány női színész van az input fájlban?\n{input.Count(x => x.gender == 1)}, {input.Count(x => x.gender == 0)}");
             
-            Console.WriteLine($"15. Hány női színész született Magyarországon?\n{input.Where(x => x.gender == 0 && x.country == "Magyarország").Count()}");
-            Console.WriteLine($"16. Hány női színész született Angliában?\n{input.Where(x => x.gender == 0 && x.country == "Anglia").Count()}");
-            Console.WriteLine($"17. Hány női színész született Skóciában?\n{input.Where(x => x.gender == 0 && x.country == "Skócia").Count()}");
-            Console.WriteLine($"18. Hány női színész született Belgiumban?\n{input.Where(x => x.gender == 0 && x.country == "Belgium").Count()}");
+            Console.WriteLine($"15. Hány női színész született Magyarországon?\n{input.Count(x => x.gender == 0 && x.country == "Magyarország")}");
+            Console.WriteLine($"16. Hány női színész született Angliában?\n{input.Count(x => x.gender == 0 && x.country == "Anglia")}");
+            Console.WriteLine($"17. Hány női színész született Skóciában?\n{input.Count(x => x.gender == 0 && x.country == "Skócia")}");
+            Console.WriteLine($"18. Hány női színész született Belgiumban?\n{input.Count(x => x.gender == 0 && x.country == "Belgium")}");
             
-            Console.WriteLine($"19. Hány férfi színész született Magyarországon?\n{input.Where(x => x.gender == 1 && x.country == "Magyarország").Count()}");
-            Console.WriteLine($"20. Hány férfi színész született Kanadában?\n{input.Where(x => x.gender == 1 && x.country == "Kanada").Count()}");
-            Console.WriteLine($"21. Hány férfi színész született USA-ban?\n{input.Where(x => x.gender == 1 && x.country == "USA").Count()}");
-            Console.WriteLine($"22. Hány férfi színész született Olaszországban?\n{input.Where(x => x.gender == 1 && x.country == "Olaszország").Count()}");
+            Console.WriteLine($"19. Hány férfi színész született Magyarországon?\n{input.Count(x => x.gender == 1 && x.country == "Magyarország")}");
+            Console.WriteLine($"20. Hány férfi színész született Kanadában?\n{input.Count(x => x.gender == 1 && x.country == "Kanada")}");
+            Console.WriteLine($"21. Hány férfi színész született USA-ban?\n{input.Count(x => x.gender == 1 && x.country == "USA")}");
+            Console.WriteLine($"22. Hány férfi színész született Olaszországban?\n{input.Count(x => x.gender == 1 && x.country == "Olaszország")}");
             
-            Console.WriteLine($"23. Hány női színész született Olaszországban vagy Spanyolországban?\n{input.Where(x => x.gender == 0 && (x.country == "Olaszország" || x.country == "Spanyolország")).Count()}");
-            Console.WriteLine($"24. Hány női színész született Magyarországon vagy Romániában?\n{input.Where(x => x.gender == 0 && (x.country == "Mexikó" || x.country == "Románia")).Count()}");
-            Console.WriteLine($"25. Hány női színész született az USA-ban vagy Mexikóban?\n{input.Where(x => x.gender == 0 && (x.country == "USA" || x.country == "Mexikó")).Count()}");
-            Console.WriteLine($"26. Hány férfi színész született Skóciában vagy Angliában?\n{input.Where(x => x.gender == 1 && (x.country == "Skócia" || x.country == "Anglia")).Count()}");
-            Console.WriteLine($"27. Hány férfi színész született Franciaországban vagy Németországban?\n{input.Where(x => x.gender == 1 && (x.country == "Franciaország" || x.country == "Németország")).Count()}");
+            Console.WriteLine($"23. Hány női színész született Olaszországban vagy Spanyolországban?\n{input.Count(x => x.gender == 0 && (x.country == "Olaszország" || x.country == "Spanyolország"))}");
+            Console.WriteLine($"24. Hány női színész született Magyarországon vagy Romániában?\n{input.Count(x => x.gender == 0 && (x.country == "Mexikó" || x.country == "Románia"))}");
+            Console.WriteLine($"25. Hány női színész született az USA-ban vagy Mexikóban?\n{input.Count(x => x.gender == 0 && (x.country == "USA" || x.country == "Mexikó"))}");
+            Console.WriteLine($"26. Hány férfi színész született Skóciában vagy Angliában?\n{input.Count(x => x.gender == 1 && (x.country == "Skócia" || x.country == "Anglia"))}");
+            Console.WriteLine($"27. Hány férfi színész született Franciaországban vagy Németországban?\n{input.Count(x => x.gender == 1 && (x.country == "Franciaország" || x.country == "Németország"))}");
 
-            Console.WriteLine($"28. Hány színész született 1950-ben vagy 1951-ben, USA-ban vagy Kanadában?\n{input.Where(x => (x.birth.Year == 1950 || x.birth.Year == 1951) && (x.country == "USA" || x.country == "Kanada")).Count()}");
-            Console.WriteLine($"29. Hány színész született 1955-ben vagy 1957-ben, Magyarországon vagy Kanadában?\n{input.Where(x => (x.birth.Year == 1955 || x.birth.Year == 1957) && (x.country == "Magyarország" || x.country == "Kanada")).Count()}");
-            Console.WriteLine($"30. Hány színész született 1961-ben vagy 1962-ben, Angliában vagy Skóciában?\n{input.Where(x => (x.birth.Year == 1961 || x.birth.Year == 1962) && (x.country == "Anglia" || x.country == "Skócia")).Count()}");
-            Console.WriteLine($"31. Hány színész született 1970-ben vagy 1971-ben, USA-ban vagy Angliában?\n{input.Where(x => (x.birth.Year == 1970 || x.birth.Year == 1971) && (x.country == "USA" || x.country == "Anglia")).Count()}");
+            Console.WriteLine($"28. Hány színész született 1950-ben vagy 1951-ben, USA-ban vagy Kanadában?\n{input.Count(x => (x.birth.Year == 1950 || x.birth.Year == 1951) && (x.country == "USA" || x.country == "Kanada"))}");
+            Console.WriteLine($"29. Hány színész született 1955-ben vagy 1957-ben, Magyarországon vagy Kanadában?\n{input.Count(x => (x.birth.Year == 1955 || x.birth.Year == 1957) && (x.country == "Magyarország" || x.country == "Kanada"))}");
+            Console.WriteLine($"30. Hány színész született 1961-ben vagy 1962-ben, Angliában vagy Skóciában?\n{input.Count(x => (x.birth.Year == 1961 || x.birth.Year == 1962) && (x.country == "Anglia" || x.country == "Skócia"))}");
+            Console.WriteLine($"31. Hány színész született 1970-ben vagy 1971-ben, USA-ban vagy Angliában?\n{input.Count(x => (x.birth.Year == 1970 || x.birth.Year == 1971) && (x.country == "USA" || x.country == "Anglia"))}");
             
-            Console.WriteLine($"32. Hány férfi színész született 1950-ben vagy 1951-ben, USA-ban vagy Kanadában?\n{input.Where(x => (x.birth.Year == 1950 || x.birth.Year == 1951) && (x.country == "USA" || x.country == "Kanada") && x.gender == 1).Count()}");
-            Console.WriteLine($"33. Hány női színész született 1955-ben vagy 1957-ben, Magyarországon vagy Kanadában?\n{input.Where(x => (x.birth.Year == 1955 || x.birth.Year == 1957) && (x.country == "Magyarország" || x.country == "Kanada") && x.gender == 0).Count()}");
-            Console.WriteLine($"34. Hány férfi színész született 1961-ben vagy 1962-ben, Angliában vagy Skóciában?\n{input.Where(x => (x.birth.Year == 1961 || x.birth.Year == 1962) && (x.country == "Anglia" || x.country == "Skócia") && x.gender == 1).Count()}");
-            Console.WriteLine($"35. Hány női színész született 1970-ben vagy 1971-ben, USA-ban vagy Angliában?\n{input.Where(x => (x.birth.Year == 1970 || x.birth.Year == 1971) && (x.country == "USA" || x.country == "Anglia") && x.gender == 0).Count()}");
+            Console.WriteLine($"32. Hány férfi színész született 1950-ben vagy 1951-ben, USA-ban vagy Kanadában?\n{input.Count(x => (x.birth.Year == 1950 || x.birth.Year == 1951) && (x.country == "USA" || x.country == "Kanada") && x.gender == 1)}");
+            Console.WriteLine($"33. Hány női színész született 1955-ben vagy 1957-ben, Magyarországon vagy Kanadában?\n{input.Count(x => (x.birth.Year == 1955 || x.birth.Year == 1957) && (x.country == "Magyarország" || x.country == "Kanada") && x.gender == 0)}");
+            Console.WriteLine($"34. Hány férfi színész született 1961-ben vagy 1962-ben, Angliában vagy Skóciában?\n{input.Count(x => (x.birth.Year == 1961 || x.birth.Year == 1962) && (x.country == "Anglia" || x.country == "Skócia") && x.gender == 1)}");
+            Console.WriteLine($"35. Hány női színész született 1970-ben vagy 1971-ben, USA-ban vagy Angliában?\n{input.Count(x => (x.birth.Year == 1970 || x.birth.Year == 1971) && (x.country == "USA" || x.country == "Anglia") && x.gender == 0)}");
             
             Console.WriteLine($"36. Mikor született a legidősebb színész?\n{input.Max(x => x.birth)}");
             Console.WriteLine($"37. Mikor született a legfiatalabb színész?\n{input.Min(x => x.birth)}");
 
             Console.WriteLine("38. Írja ki a legidősebb színész(ek) nevét és születési évét!");
-            foreach (Actor actor in MaxBy(input, (a,b) => a.birth > b.birth ? 1 : a.birth == b.birth ? 0 : -1)) {
+            foreach (Actor actor in MaxBy(input, (a,b) => a.birth < b.birth ? 1 : a.birth == b.birth ? 0 : -1)) {
                 Console.WriteLine(actor.name + ": " + actor.birth.Year);
             }
             Console.WriteLine("39. Írja ki a legfiatalabb színész(ek) nevét és születési évét!");
