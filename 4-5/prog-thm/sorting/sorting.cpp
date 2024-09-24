9ddd8872db15dd611cd424792fe63ccd09b785d9
khun2 <khunor22@gmail.com>
2024-09-24 14:12:12 +0200

    2024. szept. 24., kedd, 14:12:12 CEST

--- 4-5/prog-thm/sorting/sorting.cpp
+++ 4-5/prog-thm/sorting/sorting.cpp
@@ -1,19 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int smallest_innentől(vector<int>& v, int i) {
-    int n = i;
-    for(i++;i < v.size(); i++)
-        if(v[i] < v[n]) n = i;
-    return i;
+void writer(vector<int>& v) {
+    for(int x : v) {
+        cout << x << ' ';
+    }
+    cout << '\n';
 }
 
 void csere(vector<int>& v, int i1, int i2) {
-    if(i1==12) return;
+    if(i1==i2) return;
     int temp=v[i1];
     v[i1] = v[i2];
     v[i2] = temp;
     return;
+}
+
+int smallest_innentől(vector<int>& v, int i) {
+    int n = i;
+    for(i++;i < v.size(); i++)
+        if(v[i] < v[n]) n = i;
+    return n;
+}
+
+void insert(vector<int>& v, int i) {
+    while (0 < i && v[i-1] > v[i]) {
+        csere(v, i - 1, i);
+        i--;
+    }
 }
 
 void SelSort(vector<int>& v) {
@@ -24,14 +38,33 @@ void SelSort(vector<int>& v) {
     }
 }
 
-void writer(vector<int>& v) {
-    for(int x : v) {
-        cout << x << ' ';
+void insertion_sort(vector<int>& v) {
+    for(int i = 1; i < v.size(); i++)
+        insert(v, i);
+}
+
+bool bubbler(vector<int>& v, int n) {
+    int i = 1;
+    while(i < n-1) {
+        if (v[i-1] > v[i]) csere(v, i-1, i);
+        i++;
+    }
+    if (v[i-1] > v[i]) {
+        csere(v, i-1, i);
+        return true;
+    }
+    return false;
+}
+
+void bubble_sort(vector<int>& v) {
+    for(int i = v.size()-1; i > 0; i--){
+        if(!bubbler(v, i+1)) i--;
     }
 }
 
+
 int main() {
     vector<int> v{1,4,2,5,2,6,43,6,3,2,9,4};
-    SelSort(v);
+    bubble_sort(v);
     writer(v);
 }
