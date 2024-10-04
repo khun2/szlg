0cf6da55c4b3b18246c559baf8002dce02e4d29a
khun2 <khunor22@gmail.com>
2024-10-04 15:47:17 +0200

    2024. okt. 4., péntek, 15:47:17 CEST

--- 4-5/Moho/Alválalkozó.cpp
+++ 4-5/Moho/Alválalkozó.cpp
@@ -2,18 +2,16 @@
 using namespace std;
 
 int main() {
-	int n, day=0, num;
-	cin >> n;
-	vector<int> r;
-	for (int i = 1 ; i <= n ; i++) {
-		cin >> num;
-		if(num > day) {
-			day++;
-			r.push_back(i);
-		}
-	}
-	cout << r.size() << '\n';
-	for (int x : r) {
-		cout << x << ' ';
-	}
+    int n, day = 0, num;
+    cin >> n;
+    vector<int> r;
+    for (int i = 1; i <= n; i++) {
+        cin >> num;
+        if (num > day) {
+            day++;
+            r.push_back(i);
+        }
+    }
+    cout << r.size() << '\n';
+    for (int x : r) { cout << x << ' '; }
 }
--- 4-5/Moho/Vasut.cpp
+++ 4-5/Moho/Vasut.cpp
@@ -1,23 +1,19 @@
 #include <bits/stdc++.h>
-#include <iostream>
-#include <vector>
 using namespace std;
 
-int main(){
-	int n,k,last;
-	cin >> n >> k;
-	vector<int> v(n), r(1,1);
-	for (int i = 0; i < n; i++) {
-		cin >> v[i];
-	}
-	for (int i = 1; i < n - 1; i++) {
-		if(v[i] - v[r[r.size()-1]-1]>= k){
-			r.push_back(i+1);
-		}
-	}
-	r[r.size() - 1] = n;
-	cout << r.size() << '\n';
-	for (int x : r){
-		cout << ' ' << x;	
-	}
+int main() {
+    int n, k, last;
+    cin >> n >> k;
+    vector<int> v(n), r(1, 1);
+    for (int i = 0; i < n; i++) { cin >> v[i]; }
+    last = v[0];
+    for (int i = 1; i < n; i++) {
+        if (v[i] - last >= k) {
+            r.push_back(i + 1);
+            last = v[i];
+        }
+    }
+    r[r.size() - 1] = n;
+    cout << r.size() << '\n';
+    for (int x : r) { cout << x << ' '; }
 }
