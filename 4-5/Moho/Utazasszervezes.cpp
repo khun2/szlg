79ec52e25117a5ab55ac5cfa9109b20985bccab3
khun <khunor22@gmail.com>
2024-10-14 20:52:46 +0200

    sync: 2024-10-14 20:52:46

--- 4-5/Moho/Utazasszervezes.cpp
+++ 4-5/Moho/Utazasszervezes.cpp
@@ -5,23 +5,43 @@ int main() {
     int n, k, m, c, nextFinish = 0;
     cin >> n >> k >> m >> c;
     vector<int> v(c), finishdates(0);
-    for (int i = 0; i < c; i++) {
-        cin >> v[i];
-        cerr << v[i] << '\n';
-    }
-    int result = 0, available = n;
-    cerr << "sikerült beolvasni\n\n\n";
-    for (int i = 0; i < c; i++) {
+    for (int i = 0; i < c; i++) { cin >> v[i]; }
+    int result = 0, current = 1;
+    for (int i = 0; current < c; i++) {
+        while (n > 0) {
+            current = v[i];
+            finishdates.push_back(v[i] + m - 1);
+            n--;
+            result++;
+            i++;
+            while (finishdates.size() > 0 &&
+                   nextFinish < finishdates.size() &&
+                   finishdates[nextFinish] < current) {
+                n++;
+                nextFinish++;
+            }
+        }
         while (finishdates.size() > 0 &&
-               finishdates[nextFinish] == i) {
-            available++;
+               nextFinish < finishdates.size() &&
+               finishdates[nextFinish] < current) {
+            n++;
             nextFinish++;
         }
-        if (available > 0) {
+    }
+    /*for (int i = 0; i < c; ) {
+        while (finishdates.size() > 0 &&
+               nextFinish < finishdates.size() &&
+               finishdates[nextFinish] == i) {
+            n++;
+            nextFinish++;
+        }
+        while (n > 0) {
             finishdates.push_back(v[i] + m - 1);
-            available--;
+            n--;
             result++;
+            i++;
         }
     }
+    */
     cout << result;
 }
